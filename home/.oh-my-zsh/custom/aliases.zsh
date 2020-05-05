8ec0bcafa687e76325ef110439140bcb5142aa9f
Carlos Ribeiro <mail@cadu.dev>
2020-05-05 12:10:53 -0300

    Force tmux to use xterm-256color

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,4 @@ alias cat='bat'
 alias ls="exa"
 alias find="fd"
 alias grmb='git branch --merged | grep -v "\*" | grep -v master | grep -v staging | xargs -n 1 git branch -d'
+alias tmux="TERM=xterm-256color tmux -2"
