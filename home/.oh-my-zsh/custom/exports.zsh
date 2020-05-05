ba97cce14945a251f07c50bae32def55767ed86c
Carlos Ribeiro <mail@cadu.dev>
2020-05-05 12:11:10 -0300

    Add a GOPATH

--- home/.oh-my-zsh/custom/exports.zsh
+++ home/.oh-my-zsh/custom/exports.zsh
@@ -1,6 +1,7 @@
 export EDITOR=nvim
-export RUBYOPT=-W:no-deprecated
+#export RUBYOPT=-W:no-deprecated
 export DNSIMPLE_TOKEN=N5f05m8TmNmlZVzoHky8IBOMEbrcUP9K
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export LC_ALL=en_US.UTF-8
 export GPG_TTY=$(tty)
+export GOPATH=$HOME/go
