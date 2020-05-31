4d71b40040d72eac8d41bcafbc906fcc3e8cd2ee
Cadu Ribeiro <mail@carlosribeiro.me>
2020-05-31 12:09:19 -0300

    uncomment RUBYOPT

--- home/.oh-my-zsh/custom/exports.zsh
+++ home/.oh-my-zsh/custom/exports.zsh
@@ -1,6 +1,5 @@
 export EDITOR=nvim
-#export RUBYOPT=-W:no-deprecated
-export DNSIMPLE_TOKEN=N5f05m8TmNmlZVzoHky8IBOMEbrcUP9K
+export RUBYOPT=-W:no-deprecated
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export LC_ALL=en_US.UTF-8
 export GPG_TTY=$(tty)
