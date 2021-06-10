343b661f19ea6c0e4681a2cdb739eb141f7f5acc
Cadu Ribeiro <mail@cadu.dev>
2021-06-10 20:57:05 -0300

    Move brew to OSX block

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+date >> ~/dotfiles_status
+echo "(dotfiles) starting ..." >> ~/dotfiles_status
+
 if [ "$(uname -s)" == "Darwin" ]
 then
 	echo "Setting up your Mac  👨‍💻"
@@ -8,13 +11,14 @@ then
 	if test ! $(which brew); then
 	  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
+
+	echo 'Running brew bundle'
+	brew bundle
 fi
 
 echo 'Installing Plug'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo 'Running brew bundle'
-brew bundle
 
 echo 'Installing oh-my-zsh'
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
