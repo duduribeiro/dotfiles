87cc5b96134134eec01b4d2f598f195b237609e9
Cadu Ribeiro <mail@cadu.dev>
2021-06-10 20:46:45 -0300

    Only call brew on OSX

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo "Setting up your Mac  👨‍💻"
+if [ "$(uname -s)" == "Darwin" ]
+then
+	echo "Setting up your Mac  👨‍💻"
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	# Check for Homebrew and install if we don't have it
+	if test ! $(which brew); then
+	  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
 fi
 
 echo 'Installing Plug'
