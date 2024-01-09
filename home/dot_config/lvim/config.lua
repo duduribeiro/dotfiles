279c6d13bef586b996af1e17530c391f94f06441
Cadu Ribeiro <mail@cadu.dev>
2024-01-08 22:19:04 -0300

    include lunarvim as well

---
+++ home/dot_config/lvim/config.lua
@@ -0,0 +1,8 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- See https://github.com/nvim-treesitter/nvim-treesitter/issues/3363 and https://github.com/tree-sitter/tree-sitter-ruby/issues/230
+vim.cmd("autocmd FileType ruby setlocal indentkeys-=.")
